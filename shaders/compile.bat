46382ff343ec9477fc2c3c67f1a4c601988536df
flying_sheep <a.glowacki98@gmail.com>
2025-07-12 11:08:12 +0200

    libraries

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,9 +1,9 @@
-C:\VulkanSDK\1.3.224.1\Bin\glslc.exe shader.vert -o vert.spv
-C:\VulkanSDK\1.3.224.1\Bin\glslc.exe shader.frag -o frag.spv
+glslc.exe shader.vert -o vert.spv
+glslc.exe shader.frag -o frag.spv
 
-C:\VulkanSDK\1.3.224.1\Bin\glslc.exe shader2.vert -o vert2.spv
-C:\VulkanSDK\1.3.224.1\Bin\glslc.exe shader2.frag -o frag2.spv
+glslc.exe shader2.vert -o vert2.spv
+glslc.exe shader2.frag -o frag2.spv
 
-C:\VulkanSDK\1.3.224.1\Bin\glslc.exe pointLight.vert -o pointLight.vert.spv
-C:\VulkanSDK\1.3.224.1\Bin\glslc.exe pointLight.frag -o pointLight.frag.spv
+glslc.exe pointLight.vert -o pointLight.vert.spv
+glslc.exe pointLight.frag -o pointLight.frag.spv
 pause
